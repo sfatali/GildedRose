e63cb0d5d4cd88b1ad0b4586f79fa29759a6ce8d
Sabina Fataliyeva <s.fataliyeva@iteratia.com>
2017-09-22 00:11:57 +0400

    assignment finished

--- src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
+++ src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
@@ -34,9 +34,108 @@ public class GildedRoseTest {
 		Item itemBrie = items.get(0);
 		assertEquals(11, itemBrie.getQuality());
 	}
-    
+
 	@Test
-	public void testUpdateEndOfDay() {
-		fail("Test not implemented");
+	public void testUpdateEndOfDay_AgedBrie_Quality_not_exceeds_50() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Aged Brie", 2, 50) );
+
+		// Act
+		store.updateEndOfDay();
+
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemBrie = items.get(0);
+		assertEquals(50, itemBrie.getQuality());
+	}
+
+	@Test
+	public void testUpdateEndOfDay_Sulfuras_no_change() {// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Sulfuras, Hand of Ragnaros", 2, 80) );
+
+		store.updateEndOfDay();
+
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemBrie = items.get(0);
+		assertEquals(80, itemBrie.getQuality());
+	}
+
+	@Test
+	public void testUpdateEndOfDay_Random_item_quality_never_negative() {
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Random item", 2, 0) );
+
+		store.updateEndOfDay();
+
+		List<Item> items = store.getItems();
+		Item itemBrie = items.get(0);
+		assertEquals(0, itemBrie.getQuality());
+	}
+
+	@Test
+	public void testUpdateEndOfDay_Random_item_before_sell_in() {
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Random item", 2, 10) );
+
+		store.updateEndOfDay();
+
+		List<Item> items = store.getItems();
+		Item itemBrie = items.get(0);
+		assertEquals(9, itemBrie.getQuality());
+	}
+
+	@Test
+	public void testUpdateEndOfDay_Random_item_after_sell_in() {
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Random item", -1, 10) );
+
+		store.updateEndOfDay();
+
+		List<Item> items = store.getItems();
+		Item itemBrie = items.get(0);
+		assertEquals(8, itemBrie.getQuality());
+	}
+
+
+	@Test
+	public void testUpdateEndOfDay_backstage_passes_sellin_less_than_11() {// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10) );
+
+		store.updateEndOfDay();
+
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemBrie = items.get(0);
+		assertEquals(12, itemBrie.getQuality());
+	}
+
+	@Test
+	public void testUpdateEndOfDay_backstage_passes_sellin_less_than_6() {// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10) );
+
+		store.updateEndOfDay();
+
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemBrie = items.get(0);
+		assertEquals(13, itemBrie.getQuality());
+	}
+
+	@Test
+	public void testUpdateEndOfDay_backstage_passes_concert_passed() {// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Backstage passes to a TAFKAL80ETC concert", -1, 10) );
+
+		store.updateEndOfDay();
+
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemBrie = items.get(0);
+		assertEquals(0, itemBrie.getQuality());
 	}
 }
